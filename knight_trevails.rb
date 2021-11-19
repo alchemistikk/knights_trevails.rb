d31709a973f1f158e448e307f605f518b7287974
Mike Mitchell <alchemistikk@gmail.com>
2021-11-19 13:17:04 -0500

    Fix #possible_moves to only return positions 2 or fewer rows or columns away from Knight

--- knight_trevails.rb
+++ knight_trevails.rb
@@ -1,5 +1,5 @@
 # Todo:
-# Define where on the board the knight may move. Treat all possible moves the knight could make as children in a tree
+# Treat all possible moves the knight could make as children in a tree
 # Given the knight's position and a target position, return the fewest number of
 # moves it takes to get from the knight's position to the target position
 
@@ -37,13 +37,26 @@ class Knight
     @board = Board.new.board
   end
 
+  # Need to remove elements that are impossible to move to
+  # The greatest difference for values in both x and y coord is 2
   def possible_moves(pos)
     vertices = [-17, -15, -10, -6, 6, 10, 15, 17] # All possible moves
     possible_vertices = []
     pos_index = @board.index(pos)
-    vertices.each { |vertex| possible_vertices << pos_index + vertex }
+    vertices.each do |vertex|
+      temp = pos_index + vertex
+      possible_vertices << pos_index + vertex if temp >= 0 && temp <= 64
+    end
     possible_positions = []
-    possible_vertices.each { |vertex| possible_positions << @board[vertex] }
+    possible_vertices.each do |vertex|
+      possible_positions << @board[vertex]
+    end
+    possible_positions.each do |place|
+      2.times do |i|
+        diff = pos[i] - place[i]
+        possible_positions.delete(place) if diff > 2 || diff < -2
+      end
+    end
     possible_positions
   end
 end
